7170dfd72649e0f3ffdba3f22a4868c8aa37fdf8
Ross King <rlking1985@gmail.com>
2021-10-01 11:58:34 +1000

    Create proxy initial release

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # spws-proxy
-A SharePoint 2010 Web Service Proxy Server
+
+A SharePoint 2010 Web Service Proxy Server that uses NTLM authentication.
+
+
+## Installation
+
+```bash
+$ npm i spws-proxy
+$ npm i -g nodemon
+```
+
+## Getting Started
+
+Create a **spws.proxy.config.js** file in the root of your directory.
+
+It is **strongly** recommended that you use environment variables or add *spws.proxy.config.js* .gitignore.
+
+```javascript
+const config = {
+  // The base URL of your SharePoint site
+  baseUrl: "http://contoso/",
+  // Windows username
+  username: "john.smith",
+  // Windows password
+  password: "password1",
+  // SharePoint Domain
+  domain: "xyz",
+  // Host
+  host: "localhost",
+  // Port
+  port: 3050,
+};
+
+module.exports = config;
+```
+
+## How to run commands
+
+Run directly from the command line.
+
+```bash
+$ nodemon ./node_modules/spws-proxy
+```
+
+You may find it easier to add the swps-proxy command to the scripts object in your package.json file and call it from an npm run script.
+
+```json
+{
+  "scripts": {
+    "spws-proxy": "nodemon ./node_modules/spws-proxy"
+  }
+}
+```
+
+The proxy may only need to ran for specific reasons such as testing. In the example below a npm package named **['concurrently'](https://www.npmjs.com/package/concurrently)** is used to run the proxy and jest at the same time.
+
+```json
+{
+  "scripts": {
+    "test":" concurrently \"nodemon ./node_modules/spws-proxy\" \"jest --watch\"" "",
+  }
+}
+```
---
+++ server.js
@@ -0,0 +1,86 @@
+// Libraries
+const cors = require("cors");
+const express = require("express");
+const httpntlm = require("httpntlm");
+
+// Create express App
+const app = express();
+
+// Config file
+const config = __dirname.includes("node_modules")
+  ? require("../../spws.proxy.config")
+  : require("./config");
+
+// Middleware
+app.use(cors());
+app.use(express.text({ type: "text/*" }));
+
+// Routes
+app.get("*", forward);
+app.post("*", forward);
+
+function forward(req, res) {
+  // Get target url (remove and leading slashes)
+  let url = req.url.replace(/^\//, "");
+
+  // Create corect url
+  if (!url.toLocaleLowerCase().startsWith("http:/")) url = config.baseUrl + url;
+
+  // Create headers object
+  const headers = ["Content-Type", "soapaction"].reduce((object, header) => {
+    // Get header value
+    const value = req.get(header);
+    // If value is truthy, assign to object
+    if (value) object[header] = value;
+    // Return object
+    return object;
+  }, {});
+
+  // Create payload
+  let payload = {
+    url,
+    headers: { ...headers },
+  };
+
+  // If the method is a post request
+  if (req.method === "POST") payload.body = req.body;
+
+  // Post the request
+  httpntlm[req.method === "POST" ? "post" : "get"](
+    {
+      ...payload,
+      ...{
+        username: config.username,
+        password: config.password,
+        domain: config.domain,
+      },
+    },
+    (error, response) => {
+      // Get response body
+      const body = (response || {}).body || "";
+
+      // If logging is enabled
+      if (config.logging.responses) {
+        // Log payload
+        console.log(
+          "------------------------------------------------Response------------------------------------------------"
+        );
+        console.log({
+          url,
+          request: payload.body,
+          response: body,
+          error,
+        });
+      }
+
+      // Send error
+      if (error) return res.send(error);
+      // Else, send body
+      return res.send(body);
+    }
+  );
+}
+
+app.listen(config.port, config.host, () =>
+  console.log(`Server ready and listening at ${config.host}:${config.port}`)
+);
